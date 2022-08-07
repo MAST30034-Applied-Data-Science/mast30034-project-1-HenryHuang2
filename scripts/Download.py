6bbb1b2669874c65b3cde3c972d6b37629dce5f3
HenryHuang2 <hhychngod@gmail.com>
2022-08-07 23:29:45 +1000

    Update preprocess and download

--- scripts/Download.py
+++ scripts/Download.py
@@ -3,34 +3,36 @@ import os
 
 
 output_relative_dir = '../data/raw/'
-target_dir = 'yellow_taxi_data_2019'
-YEAR = '2019'
+target_dir = 'yellow_taxi_data_'
+YEARS = ['2019', '2020']
 MONTHS = range(1, 13)
 
-if not os.path.exists(output_relative_dir + target_dir):
-    os.makedirs(output_relative_dir + target_dir)
-            
-# data output directory is `data/raw/yellow_taxi_data_2019`
-tlc_output_dir = output_relative_dir + 'yellow_taxi_data_2019'
+for year in YEARS:
+    if not os.path.exists(output_relative_dir + target_dir + year):
+        os.makedirs(output_relative_dir + target_dir + year)
+
 
 TLC_URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"#year-month.parquet
 
-for month in MONTHS:
-    month = str(month).zfill(2) 
-    print(f"Begin month {month}")
+for year in YEARS:
+    print(f"Begin year {year}")
+    for month in MONTHS:
+        month = str(month).zfill(2) 
+        print(f"Begin month {month}")
     
-    # generate url
-    url = f'{TLC_URL_TEMPLATE}{YEAR}-{month}.parquet'
-    # generate output location and filename
-    output_dir = f"{tlc_output_dir}/{YEAR}-{month}.parquet"
-    # check if the tlc_data has already been downloaded
-    if not os.path.exists(output_dir):
-        # download
-        urlretrieve(url, output_dir)
-    else:
-        print("The required tlc_data has already been downloaded")
+        # generate url
+        url = f'{TLC_URL_TEMPLATE}{year}-{month}.parquet'
+        # generate output location and filename
+        output_dir = f"{output_relative_dir + target_dir + year}/{year}-{month}.parquet"
+        # check if the tlc_data has already been downloaded
+        if not os.path.exists(output_dir):
+            # download
+            urlretrieve(url, output_dir)
+        else:
+            print("The required tlc_data has already been downloaded")
     
-    print(f"Completed month {month}")
+        print(f"Completed month {month}")
+    print(f"Completed year {year}")
     
     
     
